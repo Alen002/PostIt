7932bfd6f7a744e77b632a65349c18f526914618
Alen <Alenslo@gmx.net>
2019-01-20 19:52:29 +0100

    Blog is showing after user identification

--- script.js
+++ script.js
@@ -2,34 +2,69 @@ var database = [
     {
         username: "Alen",
         password: "123"
-    }
+    },
+    {
+        username: "Mark",
+        password: "superuser"
+    },
+    {
+        username: "Naoki",
+        password: "japan01"
+    },
 ];
 
 var newsFeed = [
     {
-        username: "Sarah",
+        username: "Alen",
         timeline: "Travelling right now in SEA"
     },
     {
         username: "Mark",
         timeline: "Yesterday I have finished my exam lessons"
-    }
+    },
+    {
+        username: "Naoki",
+        timeline: "My country is beautiful"
+    },
+
 ];
 
 var userNamePrompt = prompt("Please enter your username");
 var userPasswordPrompt = prompt("Please enter your password");
 
 
-function signIn(username, password) {
-    if (username === database[0].username && password === database[0].password){
-        for (i=0; newsFeed.length; i++){
-            document.write(newsFeed[i].username + '</br>')
-            document.write(newsFeed[i].timeline + '</p>');
+function signIn(name, password) {
+    for (index=0; index < database.length; index++ ) {
+        if (name === database[index].username && password === database[index].password ) {
+            return true; 
+        } 
+    }
+    return false;
+}
+
+var result = signIn(userNamePrompt,userPasswordPrompt);
+
+function showFeed(checkResult, userName) {
+    if( checkResult === true) {
+        for (x=0; x < newsFeed.length; x++) {
+            if (userName === newsFeed[x].username) {
+                console.log(newsFeed[index].timeline);
             }
         }
-    else {
-        alert("Wrong User Name");
+    } else {
+        console.log("No user");
     }
 }
 
-signIn(userNamePrompt, userPasswordPrompt);
+showFeed(result, userNamePrompt);
+
+
+
+
+
+
+
+
+
+
+
